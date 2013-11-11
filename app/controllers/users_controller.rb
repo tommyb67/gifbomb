74c5bf2336d11993bce99bccf79742eb4d7fb1e7
DrRobotmck <mckenneth.scott@icloud.com>
2013-11-11 14:31:22 -0500

    added session helper, controller, user controller and updated routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+
+  before_action :authenticated!, :set_user, :authorized!, except: [:new, :create]
+
+  def index
+    @user = User.all
+  end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render status: 400, nothing: true
+    end
+
+  end
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render status: 200, nothing: true
+    else
+      render status: 400, nothing: true
+    end
+
+  end
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      session[:user_id] = nil
+      render json: {}
+    else
+      render status: 400, nothing: true
+    end
+
+  end
+
+
+# whitelisting
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def authorized!
+    unless @user.id == session[:user_id]
+    end
+  end
+  
+end
