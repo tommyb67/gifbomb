78322eaf079efa8ddef9c4c3f5f8287f77ebe0c9
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-13 11:36:25 -0500

    crises averted login was broken for a moment

--- app/assets/javascripts/userPages.js
+++ app/assets/javascripts/userPages.js
@@ -11,12 +11,11 @@ function newUserForm(){
   var form = $("<form>");
   var username = $("<input>").attr("type", "text").attr("name","username").attr("placeholder", "Username");
   var email = $("<input>").attr("type","text").attr("name","email").attr("placeholder","Email");
-  var avatar = $("<input>").attr("type","text").attr("name","avatar").attr("placeholder","Avatar URL");
   var password = $("<input>").attr("type","password").attr("name","password").attr("placeholder","Password");
   var password_confirmation = $("<input>").attr("type","password").attr("name", "password_confirmation").attr("placeholder","Confirm");
   var button = $("<button>").html("Bueno!");
 
-  form.append(username).append(email).append(avatar).append(password).append(password_confirmation).append(button).appendTo($("div.sect_three"));
+  form.append(username).append(email).append(password).append(password_confirmation).append(button).appendTo($("div.sect_three"));
 
   form.on("submit", function(event){
     event.preventDefault();
@@ -36,36 +35,7 @@ function logIn(){
   var logInForm = $("<form>");
   var emailLogin = $("<input>").attr("type","text").attr("name","email").attr("placeholder","Email");
   var passWord = $("<input>").attr("type","password").attr("name","password").attr("placeholder","Password");
-  var$(function(){
-  bindCheckBoxes($("li input[type='checkbox']"));
-  bindDeleteButtons($("li span"));
-  bindForm();
-});
-
-function bindCheckBoxes(boxes) {
-  boxes.on('change', function() {
-    var todo = $(this).parent();
-    var done = $(this).is(':checked');
-
-    done ? todo.addClass("done") : todo.removeClass("done");
-
-    updateTodo(todo, done);
-  });
-}
-
-function bindDeleteButtons(buttons) {
-  buttons.on("click", function(e) {
-    var todo = $(this).parent();
-
-    deleteTodo(todo);
-  });
-}
-
-function bindForm() {
-  $("form").on("submit", function(e) {
-    e.preventDefault();
-
- logInButton = $("<button>").html("Wilkommen");
+  var logInButton = $("<button>").html("Wilkommen");
 
   logInForm.append(emailLogin).append(passWord).append(logInButton);
   logInForm.appendTo($("div.sect_three"));
