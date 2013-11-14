9a78c3c62a021b6e0d5f533104d73de2dc53bc14
Julie lappano <julie.lappano@gmail.com>
2013-11-13 20:22:33 -0500

    added avatar field back into form

--- app/assets/javascripts/userPages.js
+++ app/assets/javascripts/userPages.js
@@ -10,11 +10,12 @@ function newUserForm(){
   var form = $("<form>");
   var username = $("<input>").attr("type", "text").attr("name","username").attr("placeholder", "Username");
   var email = $("<input>").attr("type","text").attr("name","email").attr("placeholder","Email");
+  var avatar = $("<input>").attr("type","text").attr("name","avatar").attr("placeholder","Avatar URL");
   var password = $("<input>").attr("type","password").attr("name","password").attr("placeholder","Password");
   var password_confirmation = $("<input>").attr("type","password").attr("name", "password_confirmation").attr("placeholder","Confirm");
   var button = $("<button>").html("Bueno!");
 
-  form.append(username).append(email).append(password).append(password_confirmation).append(button).appendTo($("div.sect_three"));
+  form.append(username).append(email).append(avatar).append(password).append(password_confirmation).append(button).appendTo($("div.sect_three"));
 
   form.on("submit", function(event){
     event.preventDefault();
