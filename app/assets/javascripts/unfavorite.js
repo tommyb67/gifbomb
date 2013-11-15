645f9bb102e0a1fc1df751025f44a3246e544bae
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-15 10:32:58 -0500

    remove the unfavotrite option

--- app/assets/javascripts/unfavorite.js
+++ app/assets/javascripts/unfavorite.js
@@ -1,17 +1,17 @@
 
-function destructo(gifId){
-  var unFave = $("button.unfavorite");
-  console.log(unFave);
-  unFave.on("click", function(event){
-    event.preventDefault();
-    console.log(gifId);
-    $.ajax({
-      url: "/gifs/" + gifId +"/unfavorite",
-      type: "DELETE",
-      dataType: "json",
-      data: {"_method": "unfavorite"},
-      context: this
-    });
-    $(this).parent().parent().remove();
-  });
-}
+// function destructo(gifId){
+//   var unFave = $("button.unfavorite");
+//   console.log(unFave);
+//   unFave.on("click", function(event){
+//     event.preventDefault();
+//     console.log(gifId);
+//     $.ajax({
+//       url: "/gifs/" + gifId +"/unfavorite",
+//       type: "DELETE",
+//       dataType: "json",
+//       data: {"_method": "unfavorite"},
+//       context: this
+//     });
+//     $(this).parent().parent().remove();
+//   });
+// }
